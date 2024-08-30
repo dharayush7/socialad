8277954dd2ec50bc225c87f9307f14cc91e5ae2c
contact@ayushdhar.com <AYUSH DHAR>
2024-08-30 22:59:51 +0530

    Create next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import localFont from "next/font/local";
+import "./globals.css";
+import ReactQueryProvider from "./ReactQueryProvider";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | socialad",
+    default: "socialad",
+  },
+  description: "The social media app for powernerds",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`} style={{fontFamily: "var(--font-geist-sans)"}}>
+        <ReactQueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ReactQueryProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
---
+++ src/components/posts/Post.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useSession } from "@/app/(main)/SessionProvider";
+import { PostData } from "@/lib/types";
+import { formatRelativeDate } from "@/lib/utils";
+import Link from "next/link";
+import Linkify from "../Linkify";
+import UserAvatar from "../UserAvatar";
+import UserTooltip from "../UserTooltip";
+import PostMoreButton from "./PostMoreButton";
+
+interface PostProps {
+  post: PostData;
+}
+
+export default function Post({ post }: PostProps) {
+  const { user } = useSession();
+
+  return (
+    <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flex justify-between gap-3">
+        <div className="flex flex-wrap gap-3">
+          <UserTooltip user={post.user}>
+            <Link href={`/users/${post.user.username}`}>
+              <UserAvatar avatarUrl={post.user.avatarUrl} />
+            </Link>
+          </UserTooltip>
+          <div>
+            <UserTooltip user={post.user}>
+              <Link
+                href={`/users/${post.user.username}`}
+                className="block font-medium hover:underline"
+              >
+                {post.user.displayName}
+              </Link>
+            </UserTooltip>
+            <Link
+              href={`/posts/${post.id}`}
+              className="block text-sm text-muted-foreground hover:underline"
+            >
+              {formatRelativeDate(post.createdAt)}
+            </Link>
+          </div>
+        </div>
+        {post.user.id === user.id && (
+          <PostMoreButton
+            post={post}
+            className="opacity-0 transition-opacity group-hover/post:opacity-100"
+          />
+        )}
+      </div>
+      <Linkify>
+        <div className="whitespace-pre-line break-words">{post.content}</div>
+      </Linkify>
+    </article>
+  );
+}
