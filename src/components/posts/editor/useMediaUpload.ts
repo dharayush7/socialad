a102e4b9d0943cefc764780dabf7f536aabe12d8
contact@ayushdhar.com <AYUSH DHAR>
2024-09-06 17:30:53 +0530

    set up upload media in post

---
+++ src/components/posts/editor/useMediaUpload.ts
@@ -0,0 +1,95 @@
+import { useToast } from "@/components/ui/use-toast";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useState } from "react";
+
+export interface Attachment {
+  file: File;
+  mediaId?: string;
+  isUploading: boolean;
+}
+
+export default function useMediaUpload() {
+  const { toast } = useToast();
+
+  const [attachments, setAttachments] = useState<Attachment[]>([]);
+  const [uploadProgress, setUploadProgress] = useState<number>();
+
+  const { startUpload, isUploading } = useUploadThing("attachment", {
+    onBeforeUploadBegin(files) {
+      const renamedFiles = files.map((file) => {
+        const extention = file.name.split(".").pop();
+        return new File(
+          [file],
+          `attachment_${crypto.randomUUID()}.${extention}`,
+          { type: file.type },
+        );
+      });
+
+      setAttachments((prev) => [
+        ...prev,
+        ...renamedFiles.map((file) => ({ file, isUploading: true })),
+      ]);
+
+      return renamedFiles;
+    },
+    onUploadProgress: setUploadProgress,
+    onClientUploadComplete(res) {
+      setAttachments((prev) =>
+        prev.map((a) => {
+          const uploadresult = res.find((r) => r.name === a.file.name);
+          if (!uploadresult) return a;
+          return {
+            ...a,
+            mediaId: uploadresult.serverData.mediaId,
+            isUploading: false,
+          };
+        }),
+      );
+    },
+    onUploadError(e) {
+      setAttachments((prev) => prev.filter((a) => !a.isUploading));
+      toast({
+        variant: "destructive",
+        description: e.message,
+      });
+    },
+  });
+
+  function handelStartUpload(files: File[]) {
+    if (isUploading) {
+      toast({
+        variant: "destructive",
+        description: "please wait for the current upload finish.",
+      });
+      return;
+    }
+
+    if (attachments.length + files.length > 5) {
+      toast({
+        variant: "destructive",
+        description: "You can only upload up to 5 attachments",
+      });
+      return;
+    }
+
+    startUpload(files);
+  }
+
+  function removeAttachment(fileName: string) {
+    setAttachments((prev) => prev.filter((a) => a.file.name !== fileName));
+  }
+
+  function reset() {
+    setAttachments([]);
+    setUploadProgress(undefined);
+  }
+
+  return {
+    startUpload: handelStartUpload,
+    removeAttachment,
+    reset,
+    attachments,
+    isUploading,
+    uploadProgress,
+  };
+}
