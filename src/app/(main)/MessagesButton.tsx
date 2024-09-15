dd69b72795959ae06d6532fca26a1ca3969f403e
contact@ayushdhar.com <AYUSH DHAR>
2024-09-15 19:25:51 +0530

    Google SignIn done

--- src/app/(main)/MessagesButton.tsx
+++ src/app/(main)/MessagesButton.tsx
@@ -36,7 +36,7 @@ export default function MessagesButton({ initialState }: MessagesButtonProps) {
             </span>
           )}
         </div>
-        <span className="hidden lg:inline">Notifications</span>
+        <span className="hidden lg:inline">Messages</span>
       </Link>
     </Button>
   );
